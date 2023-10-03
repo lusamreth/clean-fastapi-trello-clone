4d14043a7ed47e62bbbd17d28a740f7c88e9c4ca
lusamreth <somrethoverkill@gmail.com>
2023-10-03 16:44:45 +0700

    using config based from settings

--- src/database/main.py
+++ src/database/main.py
@@ -4,27 +4,49 @@ from sqlalchemy.orm import (
     DeclarativeBase,
 )
 from sqlalchemy import MetaData, create_engine, text
+from configs.settings import getSettings
 
 database = "jrello"
-url = "mysql+mysqldb://lusomreth:Somreth012618653@127.0.0.1:3306/{}".format(
-    database
-)
+
+
+def databaseFormat(host, port, username, password, dialect):
+    # 127.0.0.1
+    db_url = f"{dialect}://{username}:{password}@{host}:{port}"
+    return db_url
+
 
 Base = declarative_base()
 
+dbSettings = getSettings()
+DATABASE_URL = databaseFormat(
+    host=dbSettings.DATABASE_HOSTNAME,
+    password=dbSettings.DATABASE_PASSWORD,
+    port=dbSettings.DATABASE_PORT,
+    username=dbSettings.DATABASE_USERNAME,
+    dialect=dbSettings.DATABASE_DIALECT,
+)
+
 
 class Database:
-    def __init__(self, url=url):
-        self.engine = create_engine(url, echo=True)
+    def __init__(self, url=DATABASE_URL):
+        self.engine = create_engine(
+            url, echo=True, isolation_level="AUTOCOMMIT"
+        )
+        self.engine_url = url
         self.autoCreateDB()
         self.initializeDB()
 
     def autoCreateDB(self):
         with self.engine.connect() as conn:
+            conn.execute(text("commit"))
             conn.execute(
                 text(f"CREATE DATABASE IF NOT EXISTS {database}")
             )
             conn.execute(text(f"USE {database}"))
+            self.engine_url = "{}/{}".format(
+                self.engine_url, database
+            )
+            self.engine = create_engine(self.engine_url)
 
     def initializeDB(self):
         Base.metadata.create_all(bind=self.engine, checkfirst=True)
