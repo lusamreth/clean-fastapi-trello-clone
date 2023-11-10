83a134cf4ae5d48c3a710e254709ff0aa72b352c
lusamreth <somrethoverkill@gmail.com>
2023-11-10 23:22:11 +0700

    add item filter in fetch and base_id in base repo

--- src/repository/adapters/base_sql_repo.py
+++ src/repository/adapters/base_sql_repo.py
@@ -8,39 +8,48 @@ import inspect
 T = TypeVar("T")
 
 
+def clearNone(original: dict):
+    filtered = {k: v for k, v in original.items() if v is not None}
+    original.clear()
+    original.update(filtered)
+    return filtered
+
+
 class BaseRepo(BaseRepository[T]):
     def __init__(
         self,
-        session_factory: Callable[
-            ..., AbstractContextManager[Session]
-        ],
+        session_factory: Callable[..., AbstractContextManager[Session]],
         model,
         primary_key_identifier="id",
     ) -> None:
         self.session_factory = session_factory
         self.model = model
         self.identifier = primary_key_identifier
+        self.auto_model_conversion = False
 
     def add(self, **schema):
         query = self.model(**schema)
+        # if self.auto_model_conversion:
+        #     query = schema
         with self.session_factory() as session:
             try:
                 session.add(query)
                 session.commit()
                 session.refresh(query)
             except IntegrityError as error:
-                raise Exception(
-                    "Duplicate key found\nDetail: {}".format(error)
-                )
+                raise Exception("Duplicate key found\nDetail: {}".format(error))
 
         return query
 
     def update(self, id, patches):
+        if isinstance(patches, dict):
+            filteredPatches = clearNone(patches)
+        else:
+            filteredPatches = clearNone(patches.dict())
         with self.session_factory() as session:
             session.query(self.model).filter(
-                self.model.id == id
-            ).update(patches.dict())
-
+                self.model.__dict__[self.identifier] == id
+            ).update(filteredPatches)
             session.commit()
 
     def get(self, id: str) -> T | None:
@@ -59,11 +68,16 @@ class BaseRepo(BaseRepository[T]):
         with self.session_factory() as session:
             query = (
                 session.query(self.model)
-                .filter(self.model.id == id)
+                .filter(self.model.__dict__[self.identifier] == id)
                 .first()
             )
-            session.delete(query)
+
+            if query is None:
+                raise Exception("Element Not found!")
+
+            isDel = session.delete(query)
             session.commit()
+            return isDel
 
 
 # BaseRepo()
--- src/repository/adapters/cabinet_repo.py
+++ src/repository/adapters/cabinet_repo.py
@@ -1,12 +1,10 @@
-import datetime
-from sqlalchemy import DateTime
-from core.base_repo import BaseRepository
-from domains.cabinet import Board, Cabinet
+from domains.cabinet import Cabinet
 from repository.adapters.base_sql_repo import BaseRepo
 from repository.model.cabinet import BoardSchema, CabinetSchema
 from ..protocols.cabinet_repo_meta import CabinetRepo
 
-class CabinetRepoImpl(BaseRepo[CabinetSchema],CabinetRepo):
+
+class CabinetRepoImpl(BaseRepo[CabinetSchema], CabinetRepo):
     def __init__(self, session_factory):
         super().__init__(
             model=CabinetSchema,
@@ -16,37 +14,55 @@ class CabinetRepoImpl(BaseRepo[CabinetSchema],CabinetRepo):
 
     def get_all_by_topic(self, topic: str):
         with self.session_factory() as session:
-            bulks = session.query(self.model).filter(
-                    self.model.topic == topic).all()
+            bulks = session.query(self.model).filter(self.model.topic == topic).all()
             return bulks
 
-    def get_all_by_user_id(self,user_id):
+    def get_all_by_user_id(self, user_id) -> list[Cabinet]:
         with self.session_factory() as session:
-            bulks = session.query(self.model).filter(
-                    self.model.author == user_id).all()
-            return bulks
+            bulks = session.query(self.model).filter(self.model.author == user_id).all()
 
-    def db_to_entity(self, db_data : CabinetSchema | None) -> Cabinet | None:
+            def _conv(model):
+                ent = self.db_to_entity(model)
+                if ent is None:
+                    raise Exception("")
+                return ent
+
+            return list(map(_conv, bulks))
+
+    def db_to_entity(self, db_data: CabinetSchema | None) -> Cabinet | None:
         def schemaConverter(board_schema: BoardSchema) -> str:
             return board_schema.board_id
 
-        if db_data is not None: 
+        if db_data is not None:
             return Cabinet(
                 cabinetId=db_data.cabinet_id,
                 name=db_data.name,
                 author=db_data.author,
                 # boardRefs=list(map(schemaConverter,db_data.board_id_refs)),
-                boardRefs=list(map(schemaConverter,[])),
-                # createdOn=float(db_data.created_on)
-                createdOn=float(1)
+                boardRefs=list(map(schemaConverter, [])),
+                createdOn=float(db_data.created_on.timestamp()),
             )
-            
-    def entity_to_db(self, cabinet_domain: Cabinet | None) -> CabinetSchema | None:
-        if cabinet_domain is not None: 
+
+    def entity_to_db(
+        self, cabinet_domain: Cabinet | None, to_dict=False
+    ) -> CabinetSchema | dict | None:
+        if cabinet_domain is not None:
             cd = cabinet_domain
-            return CabinetSchema(cabinet_id=cd.cabinetId, 
-                                 name=cd.name,
-                                 author=cabinet_domain.author,
-                                 board_id_refs=[],
-                                 )
-    
+            _dict_res = {
+                "cabinet_id": cd.cabinetId,
+                "name": cd.name,
+                "author": cd.author,
+                "board_id_refs": [],
+            }
+
+            if not to_dict:
+                return CabinetSchema(**_dict_res)
+            else:
+                return _dict_res
+
+    def updateBoardRefs(self, boardRef):
+        pass
+
+    # def update(self, cabinet_id : str,detail: CabinetPatcher):
+    #     print("detail",detail)
+    #     return "true"
