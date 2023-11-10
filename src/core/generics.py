06d781833dbddb75615bcb5fd11f94aace2d1ae5
lusamreth <somrethoverkill@gmail.com>
2023-11-10 23:24:35 +0700

    rework the DTO model and add unwrapper

--- src/core/generics.py
+++ src/core/generics.py
@@ -1,23 +1,15 @@
 from enum import Enum
-from typing import Optional
+from typing import Generic, Optional, TypeVar
 from fastapi import Request
 from pydantic import BaseModel
 from oslash import Either
 from collections.abc import Callable
+from core.constants import AppErrors, ErrorTitle
+from core.exceptions import AuthError, DuplicatedError, NotFoundError,ValidationError
 
 DEFAULT_SUCCESS_MSG = "Request successfully fullfilled"
 
-class ErrorTitle(Enum):
-    DOMAIN_ERROR = "Domain Verification Error"
-    UNAUTHENTICATED = "Unauthenticated"
-    UNAUTHORIZED = "Unauthorized"
-    TOKEN_ERROR = "Token Gen Error"
-    UNKNOWN = "Unknown error"
-
-class AppErrors: 
-    AUTH = "auth_error"
-    VALIDATION = "validation"
-    EMPTY = "not_found"
+from enum import Enum
 
 
 class ServiceResult(BaseModel):
@@ -30,16 +22,75 @@ class GenericServiceException(BaseModel):
     errType : str
     errTitle : str
 
-class ServiceDTO(Either):
+
+class BaseBinaryResult(Either[ServiceResult , GenericServiceException]):
     def __init__(self, value : ServiceResult | GenericServiceException):
         self._value = value
 
-    def presenter(self):
-        if isinstance(self._value, ServiceResult):
+class ErrorDTOFactory:
+    def __init__(self):
+        self._DTO_Map = {}
+
+    def registerErrorType(self,type_name:str , err_class_init ):
+        self._DTO_Map[type_name] = err_class_init
+    
+    def create(self, key, **kwargs):
+        builder = self._DTO_Map.get(key)
+        print('builder' ,builder)
+        if not builder:
+            raise ValueError(key)
+        return builder(**kwargs)
+    
+    def fromServiceToWeb(self, wrapped_value : BaseBinaryResult):
+
+        if not isinstance(wrapped_value, BaseBinaryResult):
+            return wrapped_value
+
+        value = wrapped_value._value
+
+        if isRight(wrapped_value) and isinstance(value,ServiceResult):
             # self._value.message.
-            pass
+            return value
+        elif isLeft(wrapped_value) and isinstance(value,GenericServiceException):
+            eType = value.errType;
+            print("va",value.message)
+            createErrObjFunc = self.create(eType, 
+                                   message=value.message,
+                                   title=value.errTitle);
+            raise createErrObjFunc
+        else:
+            return wrapped_value
+
+
+
+DTO2HttpFactory = ErrorDTOFactory()
+
+DTO2HttpFactory.registerErrorType(AppErrors.AUTH, AuthError.create)
+DTO2HttpFactory.registerErrorType(AppErrors.VALIDATION, ValidationError)
+DTO2HttpFactory.registerErrorType(AppErrors.EMPTY, NotFoundError.create)
+
+
+class ServiceDTO(BaseBinaryResult):
+    def __init__(self, value : ServiceResult | GenericServiceException):
+        super().__init__(value)
+
+    def unwrap(self):
+        if isinstance(self._value, ServiceResult):
+            return self._value
         elif isinstance(self._value, GenericServiceException):
-            pass
+            return DTO2HttpFactory.fromServiceToWeb(self)
+
+    def unwrap_or(self, _alternative):
+        if isinstance(self._value, ServiceResult):
+            return self._value
+        else: 
+            return _alternative
+
+    def handleSuccess(self):
+        pass
+
+    def handleFailure(self):
+        pass
 
 class Right(ServiceDTO):
     def __init__(self, value):
@@ -63,12 +114,13 @@ class GenericHttpData(BaseModel):
     statusCode : int = 200
 
 
-
 def ok(data:BaseModel | dict, msg: Optional[str]=None) -> ServiceDTO:
-    if isinstance(data,dict):
+    if not isinstance(data, BaseModel):
         return Right(ServiceResult(message=msg or DEFAULT_SUCCESS_MSG
                                    ,data=data))
+
     return Right(ServiceResult(message=msg or DEFAULT_SUCCESS_MSG,data=data.model_dump()))
+
 
 def err(msg: str,err_type: str,title:Enum = ErrorTitle.UNKNOWN) -> ServiceDTO:
     return Left(GenericServiceException(message=msg, errType=err_type,
@@ -80,16 +132,20 @@ def isRight(result : Either) -> bool:
 def isLeft(result : Either) -> bool:
     return isinstance(result, Left)
 
-def runDomainService(domain_service : Callable, args, type_overwrite = AppErrors.VALIDATION):
+def runDomainService(domain_service : Callable, args = None, type_overwrite = AppErrors.VALIDATION):
     try:
-        return domain_service(**args)
+        return domain_service()
     except Exception as e:
+        # raise Exception(err(type_overwrite, str(e), ErrorTitle.DOMAIN_ERROR))
         return err(type_overwrite, str(e), ErrorTitle.DOMAIN_ERROR)
 
 # not yet use 
 async def processDTOPacket(request: Request,call_next):
     response = await call_next(request)
 
-    if not isinstance(response, ServiceDTO):
+    if isinstance(response, ServiceDTO):
         return response
+    else:
+        return response
+
 
