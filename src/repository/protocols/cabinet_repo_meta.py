234476f957cb4ee38fd24bfc4493de1f50f9f335
lusamreth <somrethoverkill@gmail.com>
2023-11-10 23:20:56 +0700

    fix some old cabinet repo protocol

--- src/repository/protocols/cabinet_repo_meta.py
+++ src/repository/protocols/cabinet_repo_meta.py
@@ -2,27 +2,28 @@ from core.base_repo import BaseRepository
 from abc import ABC, abstractmethod
 
 from repository.model.cabinet import CabinetSchema
-from domains.cabinet import Cabinet
+from domains.cabinet import Cabinet, CabinetPatcher
 
 
 class CabinetRepo(BaseRepository[CabinetSchema]):
-    # @abstractmethod
-    # def get_all_by_author(self, username: str) -> list[Cabinet] :
-    #     pass
-
     @abstractmethod
-    def db_to_entity(self, user_repo_data: CabinetSchema | None) -> Cabinet | None:
+    def db_to_entity(self, cabinet_repo_data: CabinetSchema | None) -> Cabinet | None:
         pass
 
     @abstractmethod
-    def entity_to_db(self, user_repo_data: Cabinet | None) -> CabinetSchema | None:
+    def entity_to_db(
+        self, cabinet_repo_data: Cabinet | None, to_dict: bool
+    ) -> CabinetSchema | dict | None:
         pass
 
-    @abstractmethod 
-    def get_all_by_topic(self,topic:str) -> list[Cabinet] :
-        pass
-     
     @abstractmethod
-    def get_all_by_user_id(self, user_id: str) -> list[Cabinet] :
+    def get_all_by_topic(self, topic: str) -> list[Cabinet]:
         pass
 
+    @abstractmethod
+    def get_all_by_user_id(self, user_id: str) -> list[Cabinet]:
+        pass
+
+    @abstractmethod
+    def update(self, board_id: str, Partial: CabinetPatcher) -> Cabinet:
+        pass
