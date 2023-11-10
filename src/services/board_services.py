45ceecc773638256679a4576551e029460433fac
lusamreth <somrethoverkill@gmail.com>
2023-11-10 23:19:25 +0700

    added board services

--- src/services/board_services.py
+++ src/services/board_services.py
@@ -1,45 +1,44 @@
-from ..core.base_service import BaseService
-from ..repository.protocols.board_repo_meta import BoardRepo
-import uuid
+from core.generics import ServiceDTO, err, ok, runDomainService, AppErrors
+from repository.protocols.board_repo_meta import BoardRepo
+from domains.board import Board, BoardPatcher
+from repository.protocols.cabinet_repo_meta import CabinetRepo
+from schemas.board import (
+    BoardBulkResult,
+    FetchBoardBulks,
+    BoardResult,
+    CreateBoardInput,
+)
+from services.cabinet_services import CabinetService
 
 
-class OperationState:
-    message: str
-    success: bool
-
-
-class FetchBoardService(BaseService):
+class BoardService:
     repo: BoardRepo
+    cabinetService: CabinetService
 
-    def __init__(self, repo):
+    def __init__(self, repo: BoardRepo, parentService: CabinetService):
         self.repo = repo
+        self.cabinet = parentService
 
-    async def execute(self, id: str):
-        result = await self.repo.get(id)
-        return result
+    def createBoard(self, board_data: CreateBoardInput) -> ServiceDTO:
+        makeDomainObject = lambda: Board.create(
+            name=board_data.name, topic=board_data.topic
+        )
+        entity = makeDomainObject()
+        print("ENTITY", entity)
+        # entity = runDomainService(makeDomainObject)
+        dbSchema = self.repo.entity_to_db(board_data.cabinetId, entity, to_dict=True)
+        self.repo.add(**dbSchema)
 
+        self.cabinet.appendBoardToCabinet(board_data.cabinetId, entity.boardId)
+        return ok(
+            BoardResult(**entity.model_dump()),
+            "successfully create a\
+                  new board!",
+        )
 
-class UpdateBoardService(BaseService):
-    repo: BoardRepo
-
-    def __init__(self, repo):
-        super().__init__()
-        self.repo = repo
-
-    async def execute(self, detail):
-        detail["id"] = uuid.uuid4().hex
-        self.repo.add(detail)
-        return {"success": True}
-
-
-class CreateBoardService(BaseService):
-    repo: BoardRepo
-
-    def __init__(self, repo):
-        super().__init__()
-        self.repo = repo
-
-    async def execute(self, detail):
-        # detail["id"] = uuid.uuid4().hex
-        result = self.repo.add(detail)
-        return {"success": True, "result": result}
+    def getAllBoard(self, fetcher: FetchBoardBulks):
+        entity = Board.create(fetcher.name, fetcher.topic)
+        # self.repo.add(self.repo.entity_to_db(fetcher.cabinet_id, entity, to_dict=True))
+        # self.cabinet.appendBoardToCabinet(fetcher.cabinet_id)
+        # self.repo.get
+        pass
