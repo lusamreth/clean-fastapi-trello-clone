42a6447fdd2eb083971095f4176c37c318e7c031
lusamreth <somrethoverkill@gmail.com>
2023-12-22 14:09:38 +0700

    (feat) add new router endpoint

--- src/api/v1/provider.py
+++ src/api/v1/provider.py
@@ -4,10 +4,14 @@ from database.main import Database
 from repository.adapters.auth_repo import AuthRepoImpl
 from repository.adapters.board_repo import BoardRepoImpl
 from repository.adapters.cabinet_repo import CabinetRepoImpl
+from repository.adapters.todo_repo import TodoRepoImpl
 from repository.adapters.user_repo import UserRepoImpl
+from repository.adapters.card_repo import CardRepoImpl
 
 from services.auth_services import AuthService
 from services.board_services import BoardService
+from services.card_services import CardService
+from services.todo_services import TodoService
 from services.user_services import UserService
 from services.cabinet_services import CabinetService
 
@@ -20,12 +24,16 @@ authImpl = AuthRepoImpl(session)
 userImpl = UserRepoImpl(session)
 cabinetImpl = CabinetRepoImpl(session)
 boardImpl = BoardRepoImpl(session)
+cardImpl = CardRepoImpl(session)
+todoImpl = TodoRepoImpl(session)
 
 
 userService = UserService(repo=userImpl, authRepo=authImpl)
 authService = AuthService(repo=authImpl)
 cabinetService = CabinetService(repo=cabinetImpl)
 boardService = BoardService(repo=boardImpl, parentService=cabinetService)
+cardService = CardService(repo=cardImpl, boardRepo=boardImpl)
+todoService = TodoService(repo=todoImpl, cardRepo=cardImpl)
 
 
 @lru_cache
@@ -46,3 +54,13 @@ def getCabinetService():
 @lru_cache
 def getBoardService():
     return boardService
+
+
+@lru_cache
+def getCardService():
+    return cardService
+
+
+@lru_cache
+def getTodoService():
+    return todoService
--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -3,12 +3,16 @@ from .endpoints.cabinet_route import cabinetRouter
 from .endpoints.user_route import userRouter
 from .endpoints.auth_router import authRouter
 from .endpoints.board_router import boardRouter
+from .endpoints.card_router import cardRouter
+from .endpoints.todo_router import todoRouter
 
 
 authPrefix = "/auth"
 userPrefix = "/user"
 cabinetPrefix = "/cabinet"
 boardPrefix = "/board"
+cardPrefix = "/card"
+todoPrefix = "/todo"
 
 
 def initializeRouter(app: FastAPI):
@@ -16,3 +20,5 @@ def initializeRouter(app: FastAPI):
     app.include_router(cabinetRouter, prefix=cabinetPrefix)
     app.include_router(authRouter, prefix=authPrefix)
     app.include_router(boardRouter, prefix=boardPrefix)
+    app.include_router(cardRouter, prefix=cardPrefix)
+    app.include_router(todoRouter, prefix=todoPrefix)
