c03197366141bc1835746405df10b65a63aa2dcd
lusamreth <somrethoverkill@gmail.com>
2024-02-04 17:46:34 +0700

    (feat) prep for core testing

---
+++ tests/test_core.py
@@ -0,0 +1,19 @@
+import unittest
+
+from src.core.generics import GenericServiceException, ServiceDTO, ServiceResult
+from src.core.utils.exception_handler import exceptionHandler
+
+
+class TestServiceDTOResolve(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_unwrap(self):
+        pass
+
+    def test_unwrap_or(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
