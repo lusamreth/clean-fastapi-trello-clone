114646e0ae204dfaf8858cafd395430f3f4c62d0
lusamreth <somrethoverkill@gmail.com>
2024-02-04 17:43:44 +0700

    (fix) add task refs to translator

--- src/repository/protocols/todo_repo_meta.py
+++ src/repository/protocols/todo_repo_meta.py
@@ -1,17 +1,26 @@
-from core.base_repo import BaseRepository
 from abc import abstractmethod
-from repository.model.todo import TodoSchema
+
+from core.base_repo import BaseRepository
 from domains.todo import Todo, TodoPatcher
+from repository.model.todo import TodoSchema
 
 
 class TodoRepo(BaseRepository[TodoSchema]):
     @abstractmethod
-    def db_to_entity(self, todo_repo_data: TodoSchema | None) -> Todo | None:
+    def db_to_entity(
+        self,
+        todo_repo_data: TodoSchema | None,
+        tasksRef: list[str] = [],
+        to_dict: bool = False,
+    ) -> Todo | None:
         pass
 
     @abstractmethod
     def entity_to_db(
-        self, board_id: str, todo_schema_data: Todo | None, to_dict: bool
+        self,
+        board_id: str,
+        todo_schema_data: Todo | None,
+        to_dict: bool,
     ) -> TodoSchema | dict | None:
         pass
 
